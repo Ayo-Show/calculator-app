f37f6e8b85437b7a9b7b7f024a875b9cef6ef392
Ayo-Show <sowunmi30@gmail.com>
2022-02-14 11:52:44 -0500

    updated function to handle operators

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function inputDigit(digit) {
   const { displayValue } = calculator;
   // Overwrite `displayValue` if the current value is '0' otherwise append to it
   calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+  console.log(calculator);
 }
 
 function inputDecimal(dot) {
@@ -17,6 +18,26 @@ function inputDecimal(dot) {
     // Append the decimal point
     calculator.displayValue += dot;
   }
+}
+
+
+function handleOperator(nextOperator) {
+  // Destructure the properties on the calculator object
+  const { firstOperand, displayValue, operator } = calculator
+  // `parseFloat` converts the string contents of `displayValue`
+  // to a floating-point number
+  const inputValue = parseFloat(displayValue);
+
+  // verify that `firstOperand` is null and that the `inputValue`
+  // is not a `NaN` value
+  if (firstOperand === null && !isNaN(inputValue)) {
+    // Update the firstOperand property
+    calculator.firstOperand = inputValue;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
 }
 
 
@@ -42,7 +63,8 @@ keys.addEventListener("click", (event) => {
   }
 
   if (target.classList.contains("operator")) {
-    console.log("operator", target.value);
+    handleOperator(target.value);
+    updateDisplay();
     return;
   }
 
