7c89708ab39eb0598c30f687b98881c457d092fe
Ayo-Show <sowunmi30@gmail.com>
2022-02-16 16:37:35 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculator App
+
+A simple calculator used to solve basic arithmetic problems, made using javascript, HTML and CSS.
+
+## Final Result
+
+![calculator]()
--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function handleOperator(nextOperator) {
   } else if (operator) {
     const result = calculate(firstOperand, inputValue, operator);
 
-    calculator.displayValue = String(result);
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
     calculator.firstOperand = result;
   }
 
